4b252e68298870494c30e54206a1b2e18d428bf7
I~am~Mim <mim@pop-os.localdomain>
2024-03-21 00:18:14 +0600

    Check build status

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,20 @@
-
 import uvicorn
-from config import get_config
 from fastapi import FastAPI, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(
-    title=str(get_config().project_title),
+    title="CICD-implementation",
     docs_url="/api/docs",
 )
 app.add_middleware(
     CORSMiddleware, allow_headers=["*"], allow_origins=["*"], allow_methods=["*"]
 )
 
+
 @app.get("/health_check")
 def read_root_health_check():
     return {"data": "success!"}
+
 
 if __name__ == "__main__":
     uvicorn.run(
